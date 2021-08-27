47a9e39ef12cc2a1182fd5fd5f31730d8b799792
s6252410017 <s6252410017@sau.ac.th>
2021-08-27 12:49:59 +0700

    Code V.2 :Second

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstUI(),   //เรียกหน้าจอแรก
+      home: SecondUI(),   //เรียกหน้าจอแรก
     ),
   );
 }
--- lib/views/second_ui.dart
+++ lib/views/second_ui.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SecondUI extends StatefulWidget {
-  const SecondUI({ Key? key }) : super(key: key);
+  const SecondUI({Key? key}) : super(key: key);
 
   @override
   _SecondUIState createState() => _SecondUIState();
@@ -11,7 +11,109 @@ class _SecondUIState extends State<SecondUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      //backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          'Second UI',
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+            ),
+          ),
+         IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Colors.grey[850],
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/images/myproflie.jpg',
+                ),
+              ),
+              accountName: Text(
+                'Terasak Treepak',
+              ),
+              accountEmail: Text(
+                'ID: 6252410017',
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/bgdrawer.png',
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              otherAccountsPictures: [
+                Image.asset(
+                  'assets/images/saulogo.png',
+                ),
+              ],
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text(
+                'หน้าแรก',
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text(
+                'ตารางสอน',
+              ),
+              leading: Icon(
+                Icons.table_chart,
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text(
+                'ตารางสอบ',
+              ),
+              leading: Icon(
+                Icons.access_alarm,
+              ),
+              trailing: Text(
+                '999',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.red,
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text(
+                'ออกจาการใช้งาน',
+              ),
+              trailing: Icon(
+                Icons.exit_to_app,
+                color: Colors.blue,
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    
     );
   }
 }
